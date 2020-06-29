2f37a3914b965d6598f5241f6182c141d10e57a3
Aycan Özyılmaz <aycanozyilmaz@gmail.com>
2020-06-29 22:45:55 +0300

    Add project files.

---
+++ DetaySayfasi.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Katalog
+{
+    /// <summary>
+    /// Interaction logic for DetaySayfasi.xaml
+    /// </summary>
+    public partial class DetaySayfasi : Page
+    {
+        public DetaySayfasi()
+        {
+            InitializeComponent();
+        }
+
+        public DetaySayfasi(object data) : this()
+        {
+            this.DataContext = data;
+        }
+    }
+}
---
+++ ListeSayfasi.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Katalog
+{
+    /// <summary>
+    /// Interaction logic for ListeSayfasi.xaml
+    /// </summary>
+    public partial class ListeSayfasi : Page
+    {
+        public ListeSayfasi()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            DetaySayfasi detaySayfasi = new DetaySayfasi(this.KonutListView.SelectedItem);
+            this.NavigationService.Navigate(detaySayfasi);
+        }
+    }
+}
